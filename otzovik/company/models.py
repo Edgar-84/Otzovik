18a97a10cff47f56451782b030b9ea46714da436
Edgar-84 <edgar.tomko.97@mail.ru>
2021-09-13 13:45:33 +0300

    create admin panel

--- otzovik/company/models.py
+++ otzovik/company/models.py
@@ -4,12 +4,13 @@ from django.urls import  reverse
 class Company(models.Model):
     """This table will store all the information about the companies"""
 
-    title = models.CharField(max_length=100)
-    url_for_site = models.URLField(max_length=100)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, verbose_name="Название")
+    url_for_site = models.URLField(max_length=100, verbose_name='Ссылка на сайт компании')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     content = models.TextField()
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -17,11 +18,21 @@ class Company(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Компании'
+        verbose_name_plural = 'Компании'
+        ordering = ['title']
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
